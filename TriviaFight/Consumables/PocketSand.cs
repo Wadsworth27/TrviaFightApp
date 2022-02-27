1e1ac3ba1c1e72dfd47f4de38082dfd63ff569f8
Wadsworth27 <stwadsworth27@gmail.com>
2022-02-26 22:02:33 -0500

    Pocket sand added

---
+++ TriviaFight/Consumables/PocketSand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaFight.Consumables
+{
+    public class PocketSand : Consumable, IConsumable
+    {
+        public override string Name { get; set; } = "PocketSand";
+
+        public override string Description { get; } = "Using your pocket sand to blind your enemy, their odds of hitting you decrease for sveral turns.";
+
+        public override string Target { get; set; } = "Enemy";
+        public override int Quantity { get; set; } = 1;
+        public override int Price { get; set; } = 100;
+        public override string ToString()
+        {
+            return Name;
+        }
+        public override void Use(Enemy enemy)
+        {
+            enemy.TemporaryStatModifiers.AddModifier(new StatModifier("HitPercentage", 4, -100));
+            Quantity--;
+            Console.WriteLine("You land a direct hit with your pocket sand. The enemy stuggles to see you.");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+        public PocketSand(int quantity) : base(quantity)
+        {
+
+        }
+
+    }
+}
--- TriviaFight/TemporaryStatBoosts/TemporaryStatModifiers.cs
+++ TriviaFight/TemporaryStatBoosts/TemporaryStatModifiers.cs
@@ -3,10 +3,12 @@
     public class TemporaryStatModifiers
     {
         public int TemporarySpeed { get; set; }
+        public int TemporaryHitPercentage { get; set; }
         private List<StatModifier> statModifiers = new List<StatModifier>();
         public void AddModifier(StatModifier statmod)
         {
             statModifiers.Add(statmod);
+            CalculateStats();
             Console.WriteLine(statModifiers.Count);
         }
         public void RemoveInactiveModifiers()
@@ -25,7 +27,9 @@
         {
             RemoveInactiveModifiers();
             TemporarySpeed = 0;
+            TemporaryHitPercentage = 0;
             int _tempSpeed = 0;
+            int _tempHitPercentage = 0;
             foreach (StatModifier modifier in statModifiers)
             {
                 switch (modifier.Stat)
@@ -33,11 +37,15 @@
                     case "Speed":
                         _tempSpeed += modifier.Modifier;
                         break;
+                    case "HitPercentage":
+                        _tempHitPercentage += modifier.Modifier;
+                        break;
                     default:
                         break;
                 }
             }
             this.TemporarySpeed += _tempSpeed;
+            this.TemporaryHitPercentage += _tempHitPercentage;
         }
         public void DecrementTurnsRemaining()
         {
